5d86558c6a79ff5afce2f723f1fe2a1795ff0881
Kenzi Jaki <69611905+kenzikahardian@users.noreply.github.com>
2021-12-19 11:58:29 +0700

    Update Mahasiswa.cs

--- FundamentalProgramming/Mahasiswa.cs
+++ FundamentalProgramming/Mahasiswa.cs
@@ -1,58 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace FundamentalProgramming
 {
-    public class Mahasiswa 
+    class Mahasiswa 
     {
-        public int Nim { get; set; }
-        public string Name { get; set; }
+        public string Nim { get; set; }
+        public string Nama { get; set; }
         public string Jurusan { get; set; }
-        public float Ipk { get; set; }
-        public int LihatMhs { get; set; }
-        public int TotalInput { get; set; }
 
-        public Mahasiswa[] mhs = new Mahasiswa[25];
-        public List<Mahasiswa> dataMhs = new List<Mahasiswa>();
-
-
-        public void LihatMahasiswa(List<Mahasiswa> l)
+        public Mahasiswa(Activity a)
         {
-            if (LihatMhs == 0)
-            {
-                Console.WriteLine("Data Mahasiswa Tidak ada");
-            }
-            else
-            {
-                Console.WriteLine("Total Karyawan:" + dataMhs.Count);
-                foreach (Mahasiswa e in l)
-                {
-                    Console.WriteLine("Id :", e.Nim);
-                    Console.WriteLine("Nama  is:", e.Name);
-                    Console.WriteLine("Jurusan:", e.Jurusan);
-                }
-            }
-
-            /*Console.WriteLine($"NIM : {Nim}");
-            Console.WriteLine($"NAMA : {Name}");
-            Console.WriteLine($"JURUSAN : {Jurusan}");
-            Console.WriteLine($"IPK : {Ipk}");*/
+            Console.Write("Masukkan NIM: ");
+            this.Nim = Console.ReadLine();
+            Console.Write("Masukkan Nama: ");
+            this.Nama = Console.ReadLine();
+            Console.Write("Masukkan Jurusan: ");
+            this.Jurusan = Console.ReadLine();
         }
-        public void TambahMahasiswa(List<string> datamhs) 
-        {
-            Console.WriteLine("Berapa banyak mahasiswa yang mau ditambah?");
-            TotalInput = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < TotalInput; i++)
-            {
-                mhs[i] = new Mahasiswa();
-                dataMhs.Add(mhs[i]);
-            }
-            Console.WriteLine("Masukkan Nama");
-            Name = Console.ReadLine();
-        }
-
+   
         
-        }
-    }
+     }
+}
 
